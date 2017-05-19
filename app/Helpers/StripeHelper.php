4679ad37ec110839f8119a90b88ddfcdb3cf43a2
enmaGit <enmanuel21498@gmail.com>
2017-05-19 10:18:10 -0400

    Push test

--- app/Helpers/StripeHelper.php
+++ app/Helpers/StripeHelper.php
@@ -13,6 +13,7 @@ class StripeHelper
 
     public static function generateCharge($token, $amount, $description)
     {
+        \Log::info('Esta es la secret key: ' . env('STRIPE_SECRET_KEY', 'NO_tE_LA_dIRE'));
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY', 'NO_tE_LA_dIRE'));
         return $charge = \Stripe\Charge::create(array(
           "amount" => $amount,
