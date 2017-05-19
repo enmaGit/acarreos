05710accb91f954b9d37fbaff1770859fbbbbf98
enmaGit <enmanuel21498@gmail.com>
2017-05-19 08:54:34 -0400

    Change token on StripeHelper

--- app/Helpers/StripeHelper.php
+++ app/Helpers/StripeHelper.php
@@ -18,7 +18,7 @@ class StripeHelper
           "amount" => $amount,
           "currency" => "usd",
           "description" => $description,
-          "card" => $token,
+          "source" => $token,
         ));
     }
 
